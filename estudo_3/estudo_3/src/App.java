b62bd4cf78ea692c166be724399483b5b2452184
Felipe pinheiro <santospinheiro627@gmail.com>
2022-07-10 19:31:52 -0300

    projeto 3

--- estudo_3/estudo_3/src/App.java
+++ estudo_3/estudo_3/src/App.java
@@ -1,9 +1,37 @@
+import java.util.Scanner;
 public class App {
+    
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        System.out.println(12);
-        System.out.println(19);
+
+            //fazer a adição de duas parcelas
+        System.out.println("Adicionar duas parcelas");
+
+            //definir variaveis
+        int parcela1;
+        int parcela2;
+        int resultado;
+
+            // criar objeto de inserção
+
+        Scanner input = new Scanner(System.in);
+
+            //adição da primeira parcela
+        System.out.println("Indique a primeira parcela:");
+        parcela1 = input.nextInt();
         
+            //adição da Segunda  parcela
+        System.out.println("Indique a segunda parcela:");
+        parcela2 = input.nextInt();
+        
+            //Resultado
+
+        resultado = parcela1 + parcela2;
+
+            // Listar resultado
+            // System.out.println(resultado);
+            // System.out.printf(" O resultado final é %d + %d = %d", parcela1 , parcela2, resultado);
+        System.out.printf("O resultado final é " +  resultado);
+
         
     }
 }
