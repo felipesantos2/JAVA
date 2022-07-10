02596b1911dd0f83a2994a35c9d64b8f9e1571b0
Felipe pinheiro <santospinheiro627@gmail.com>
2022-07-10 17:12:53 -0300

    add folder estudo_3

---
+++ estudo_3/estudo_3/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        System.out.println(12);
+        System.out.println(19);
+        
+        
+    }
+}
