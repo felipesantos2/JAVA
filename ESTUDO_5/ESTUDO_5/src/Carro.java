3614c862ccd031b07135504edf50880391233e37
Felipe pinheiro <santospinheiro627@gmail.com>
2022-07-12 22:33:08 -0300

    classes

---
+++ ESTUDO_5/ESTUDO_5/src/Carro.java
@@ -0,0 +1,27 @@
+public class Carro {
+
+    public String nome = new String();
+    public String tipo = null;
+    public int ano = 2023;
+    public int portas = 4;
+    public boolean funciona = true;
+
+    
+    
+    public void abrirportas() {
+        //code
+
+    }
+    
+    public void ligarcarro() {
+        //code
+
+    }
+    public void desligarcarro() {
+        //code
+
+    }
+
+
+
+}
