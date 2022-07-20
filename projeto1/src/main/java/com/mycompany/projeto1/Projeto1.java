92809796ffc9aa29d61e1c8e3d5ac0b5f2d92306
Felipe <santospinheiro627@gmail.com>
2022-07-19 22:50:52 -0300

    big bang

---
+++ projeto1/src/main/java/com/mycompany/projeto1/Projeto1.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.projeto1;
+
+/**
+ *
+ * @author Felipe
+ */
+public class Projeto1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+        int num1 = 20;
+        int num2 = 30;
+        if(num1 > num2 ){
+            
+            System.out.println("O valor 1 é maor que o valor 2");
+        } else {
+            System.out.println("O valor `2` é maor que o valor `1`");
+
+        }
+        
+        System.out.println("A Soma dos valores é:" + (num1+ num2));
+    } 
+}
