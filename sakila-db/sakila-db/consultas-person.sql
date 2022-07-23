65666f64365a90b7f5f14375df3b85843f04e2d4
Felipe pinheiro dos Santos <santospinheiro627@gmail.com>
2022-07-23 10:08:28 -0300

    Update consultas-person.sql

--- sakila-db/sakila-db/consultas-person.sql
+++ sakila-db/sakila-db/consultas-person.sql
@@ -1,15 +1,16 @@
 SELECT first_name, last_name, address
 FROM sakila.customer JOIN sakila.address
 ON customer.address_id = customer.address_id;
----------------------------------------------
+-------------------------------------------
 SELECT first_name, last_name, address
 FROM customer JOIN address
 ON customer.address_id = customer.address_id;
 
 --------------------------------------------
 SELECT title, name FROM sakila.film
-JOIN sakila.`language`
+INNER JOIN sakila.`language`
 ON film.language_id = language.language_id;
+---------------------------------------------
 
 
 
